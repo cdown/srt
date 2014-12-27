ab4b3a4001740572bad1fd4f991c5fb373d1e122
Chris Down <chris@chrisdown.name>
2014-12-27 20:58:37 +0000

    Add SRT parsing functionality

---
+++ tinysrt.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import re
+import datetime
+from collections import namedtuple
+
+SUBTITLE_REGEX = re.compile(r'''\
+(\d+)
+(\d+:\d+:\d+,\d+) --> (\d+:\d+:\d+,\d+)
+(.+)
+''')
+
+Subtitle = namedtuple('Subtitle', ['index', 'start', 'end', 'content'])
+
+
+def parse_time(time):
+    hours, minutes, seconds, milliseconds = map(int, re.split('[,:]', time))
+    return datetime.timedelta(
+        hours=hours,
+        minutes=minutes,
+        seconds=seconds,
+        milliseconds=milliseconds,
+    )
+
+
+def parse(srt_file_handle):
+    for match in SUBTITLE_REGEX.finditer(srt_file_handle.read()):
+        raw_index, raw_start, raw_end, content = match.groups()
+        yield Subtitle(
+            index=int(raw_index), start=parse_time(raw_start),
+            end=parse_time(raw_end), content=content,
+        )
