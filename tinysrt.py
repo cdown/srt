a8f68ff0b58af55ce05ba24cd7d6ebd932c2f91e
Chris Down <chris@chrisdown.name>
2015-01-14 23:24:51 +0000

    Map times to integers using a listcomp, not a map

--- tinysrt.py
+++ tinysrt.py
@@ -30,7 +30,8 @@ def parse_time(time):
     Parse an SRT formatted time to HH:MM:SS,ms.
     '''
 
-    hours, minutes, seconds, milliseconds = map(int, re.split('[,:]', time))
+    split_time = [int(x) for x in re.split('[,:]', time)]
+    hours, minutes, seconds, milliseconds = split_time
     return datetime.timedelta(
         hours=hours, minutes=minutes,
         seconds=seconds, milliseconds=milliseconds,
