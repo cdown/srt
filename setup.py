3e6f875545db07d9b31d72d36ad410eda40b5958
Chris Down <chris@chrisdown.name>
2015-02-28 17:29:05 +0000

    Do not list Python 2.6 as supported in PyPi classifiers

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
