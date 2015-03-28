b0b2e6d0fe5656825bc4ded8314297983f3401d9
Chris Down <chris@chrisdown.name>
2015-03-28 07:52:49 +0000

    Add basic argument parser that will be used in basic srt tools

---
+++ utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+
+def parse_basic_args(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '--input', '-i', metavar='FILE',
+        default=sys.stdin, type=argparse.FileType('r'),
+        help='the file to process (default: stdin)',
+    )
+    parser.add_argument(
+        '--output', '-o', metavar='FILE',
+        default=sys.stdout,
+        type=argparse.FileType('w'),
+        help='the file to write to (default: stdout)',
+    )
+    return parser.parse_args()
