77b10344a90e4ad1456bc209590d380de5da699d
Chris Down <chris@chrisdown.name>
2015-09-24 18:44:04 +0100

    List Python 3.5 as supported in setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     test_suite='nose.collector',
     tests_require=TEST_REQUIREMENTS
