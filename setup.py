89b8c10bd40b799952e7f26d43ab55ef15c42f67
Chris Down <chris@chrisdown.name>
2018-11-28 08:56:48 +0000

    setup.py: Add 3.6/3.7 support

--- setup.py
+++ setup.py
@@ -42,6 +42,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Multimedia :: Video",
         "Topic :: Software Development :: Libraries",
         "Topic :: Text Processing",
