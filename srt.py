7b485e2f62554d8d3165f327c1d9bb7c74b4abf5
Chris Down <chris@chrisdown.name>
2019-05-31 14:11:01 -0700

    sort_and_reindex: Make skipping optional, and document it
    This fixes #45, although instead of decoupling skipping from
    sort_and_reindex, this documents it and makes it a first class citizen.
    This is because, after more thinking, I'm fairly certain that this is
    the only place that makes sense to actually do the skipping.
    
    There should be no behavioural changes if `skip` is not passed, since we
    still check and skip by default.

--- srt.py
+++ srt.py
@@ -231,12 +231,20 @@ def srt_timestamp_to_timedelta(ts):
     return timedelta(hours=hrs, minutes=mins, seconds=secs, milliseconds=msecs)
 
 
-def sort_and_reindex(subtitles, start_index=1, in_place=False):
+def sort_and_reindex(subtitles, start_index=1, in_place=False, skip=True):
     """
     Reorder subtitles to be sorted by start time order, and rewrite the indexes
     to be in that same order. This ensures that the SRT file will play in an
     expected fashion after, for example, times were changed in some subtitles
     and they may need to be resorted.
+
+    If skip=True, subtitles will also be skipped if they are considered not to
+    be useful. Currently, the conditions to be considered "not useful" are as
+    follows:
+
+    - Content is empty, or only whitespace
+    - The start time is negative
+    - The start time is equal to or later than the end time
 
     .. doctest::
 
@@ -254,6 +262,8 @@ def sort_and_reindex(subtitles, start_index=1, in_place=False):
     :param int start_index: The index to start from
     :param bool in_place: Whether to modify subs in-place for performance
                           (version <=1.0.0 behaviour)
+    :param bool skip: Whether to skip subtitles considered not useful (see
+                      above for rules)
     :returns: The sorted subtitles
     :rtype: :term:`generator` of :py:class:`Subtitle` objects
     """
@@ -262,12 +272,15 @@ def sort_and_reindex(subtitles, start_index=1, in_place=False):
         if not in_place:
             subtitle = Subtitle(**vars(subtitle))
 
-        try:
-            _should_skip_sub(subtitle)
-        except _ShouldSkipException as thrown_exc:
-            log.warning("Skipped subtitle at index %d: %s", subtitle.index, thrown_exc)
-            skipped_subs += 1
-            continue
+        if skip:
+            try:
+                _should_skip_sub(subtitle)
+            except _ShouldSkipException as thrown_exc:
+                log.warning(
+                    "Skipped subtitle at index %d: %s", subtitle.index, thrown_exc
+                )
+                skipped_subs += 1
+                continue
 
         subtitle.index = sub_num - skipped_subs
 
