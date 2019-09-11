3ad854bbe664d8e9f102808f6124ead9d48f4f36
Chris Down <chris@chrisdown.name>
2019-09-11 19:43:51 +0100

    setup.py: Remove 3.4 from trove

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with codecs.open("README.rst", encoding="utf8") as readme_f:
 setup(
     name="srt",
     version="2.1.0",
-    description="A tiny library for parsing, modifying, and composing SRT " "files.",
+    description="A tiny library for parsing, modifying, and composing SRT files.",
     long_description=README,
     author="Chris Down",
     author_email="chris@chrisdown.name",
@@ -40,7 +40,6 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
