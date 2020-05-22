f4a552fdadd0d8516a5bdec3b3f8cc60562e1476
Chris Down <chris@chrisdown.name>
2020-05-22 22:10:23 +0100

    Revert "Drop Python 2 support, EOL as of 2020-01-01"
    For now let's keep it until 2021. I don't want to break tools like
    ffsubsync.
    
    This reverts commit 5f9c91f6d39456338bf82fbf0d9606ff22914bc5.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
 import codecs
-from distutils.core import setup
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 
 with codecs.open("README.rst", encoding="utf8") as readme_f:
     README = readme_f.read()
@@ -9,7 +14,7 @@ with codecs.open("README.rst", encoding="utf8") as readme_f:
 setup(
     name="srt",
     version="3.2.1",
-    python_requires=">=3.5",
+    python_requires=">=2.7",
     description="A tiny library for parsing, modifying, and composing SRT files.",
     long_description=README,
     author="Chris Down",
@@ -33,6 +38,8 @@ setup(
         "Intended Audience :: Developers",
         "License :: Public Domain",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
--- tests/test_srt.py
+++ tests/test_srt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding=utf8
 
 from __future__ import unicode_literals
 from datetime import timedelta
