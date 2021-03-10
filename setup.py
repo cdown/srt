bc3d43e3f4aa37f5cb9f51f809c8a66c6686b68f
Chris Down <chris@chrisdown.name>
2021-03-10 14:34:34 +0000

    support: Test on Python 3.9

--- setup.py
+++ setup.py
@@ -45,6 +45,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Multimedia :: Video",
         "Topic :: Software Development :: Libraries",
         "Topic :: Text Processing",
