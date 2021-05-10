07a29c115651f637d9761a9edcd85f368b774542
switchupcb <contact@switchupcb.com>
2021-05-09 22:58:34 -0500

    srt_tools: re-optimize srt_remove
    Re-optimized the srt-remove functionality with split unit tests included. util functions (binary search, contains) removed.

--- srt_tools/tests/test_srt_remove.py
+++ srt_tools/tests/test_srt_remove.py
@@ -66,46 +66,6 @@ class TestHelpers(unittest.TestCase):
         self.assertEqual(get_timestamp(
             self.subs(), t('00:00:11,000')), self.x[0].start)
 
-    def test_contains_timestamp(self):
-        self.assertEqual(contains_timestamp(self.r, t('00:00:10,000')), False)
-        self.assertEqual(contains_timestamp(
-            self.r, t('00:00:11,000')), True)  # start
-        self.assertEqual(contains_timestamp(
-            self.r, t('00:00:12,000')), True)  # btwn
-        self.assertEqual(contains_timestamp(
-            self.r, t('00:00:12,701')), False)  # end
-        self.assertEqual(contains_timestamp(self.r, t('00:00:13,000')), False)
-
-    def test_captions_containing_timestamp(self):
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(), t('00:00:00,000')), [])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(), t('00:00:11,000')), [self.x[0]])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(), t('00:00:12,000')), [self.x[0]])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(), t('00:00:12,701')), [self.x[1]])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(), t('00:00:35,000')), [])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(), t('00:00:16,708')), [self.x[2], self.x[3]])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(), t('00:00:17,272')), [self.x[2], self.x[4]])
-
-        # distanced overlaps
-        self.assertEqual(captions_containing_timestamp(self.subs(1), t(
-            '00:00:4,000')), [self.y[0], self.y[1], self.y[2], self.y[3]])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(1), t('00:00:9,000')), [self.y[0], self.y[5]])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(1), t('00:00:9,500')), [self.y[0], self.y[5]])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(1), t('00:00:3,450')), [self.y[0], self.y[1], self.y[2]])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(1), t('00:00:5,500')), [self.y[0], self.y[1]])
-        self.assertEqual(captions_containing_timestamp(
-            self.subs(1), t('00:00:11,000')), [])
-
 
 class TestCaptionTools(unittest.TestCase):
     def setUp(self):
@@ -146,7 +106,6 @@ class TestCaptionTools(unittest.TestCase):
         self.assertEqual(list(result), sort([self.x[0], self.x[1]]))
 
         a = sort([self.x[2], self.x[3]])
-
         result = remove_caption_index(self.subs(), -1, -3)  # reverse
         self.assertEqual(list(result), a)
 
@@ -212,6 +171,21 @@ class TestCaptionTools(unittest.TestCase):
         result = remove_caption_timestamp(
             self.subs(), self.x[0].start, self.x[0].start)
         self.assertEqual(list(result), [])
+
+    def test_split(self):
+        a = [srt.Subtitle(1, t('00:00:11,000'), t('00:00:12,701'), "A"),
+             srt.Subtitle(2, t('00:00:12,701'), t('00:00:14,203'), "B"),
+             srt.Subtitle(3, t('00:00:14,500'), t('00:00:17,500'), "C"),
+             srt.Subtitle(4, t('00:00:16,538'), t('00:00:17,272'), "D"),
+             srt.Subtitle(5, t('00:00:17,272'), t('00:00:17,500'), "E"),
+             srt.Subtitle(6, t('00:00:17,500'), t('00:00:18,440'), "E"),
+             srt.Subtitle(7, t('00:00:17,500'), t('00:00:19,738'), "C")]
+        result = split(self.subs(),  t('00:00:17,500'))
+        self.assertEqual(list(result), a)
+
+        result = split(self.subs(), self.x[0].start)
+        self.assertEqual(list(result), self.x)
+
 
 
 if __name__ == '__main__':
