1855135bef15ccd4e732404b890037660349f445
Paul Frischknecht <paul_frischknecht@bluewin.ch>
2021-08-31 14:53:11 +0200

    Fix typo in to_srt documentation (#75)

--- srt.py
+++ srt.py
@@ -143,7 +143,7 @@ class Subtitle(object):
 
         :param bool strict: If disabled, will allow blank lines in the content
                             of the SRT block, which is a violation of the SRT
-                            standard and may case your media player to explode
+                            standard and may cause your media player to explode
         :param str eol: The end of line string to use (default "\\n")
         :returns: The metadata of the current :py:class:`Subtitle` object as an
                   SRT formatted subtitle block
