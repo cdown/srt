87ad5a7bd77b2be4786ee42bd00ad936efbef4fc
Chris Down <chris@chrisdown.name>
2022-01-24 02:23:36 +0000

    trove: Add 3.10 support

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Multimedia :: Video",
         "Topic :: Software Development :: Libraries",
         "Topic :: Text Processing",
